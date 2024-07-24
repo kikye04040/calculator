503aaf4aa6f5946161ed5afe3e7e0d45f2caee44
kikye04040 <kikye04040@gmail.com>
2024-07-24 16:03:30 +0900

    level 1-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,13 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // level 1-5
+        int[] resultArray = new int[10];
+        int index = 0;
+        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
+
+        // level 1-4
         while (true) {
             // level 1-1
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -45,6 +52,11 @@ public class App {
                 continue;
             }
             System.out.println("결과: " + result);
+
+            // level 1-5
+            resultArray[index] = result;
+            index++;
+            System.out.println(index);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String endWord = sc.next();
