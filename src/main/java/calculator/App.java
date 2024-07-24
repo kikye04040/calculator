86ef8d005685df152789cca60f0b7c00a8c6042f
kikye04040 <kikye04040@gmail.com>
2024-07-24 15:06:42 +0900

    level 1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,9 +6,14 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // level 1-1
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
+
+        // level 1-2
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char cal = sc.next().charAt(0);
     }
 }
