f35e73f0aed05f4b500c6a6ccf878619e089b1f2
kikye04040 <kikye04040@gmail.com>
2024-07-24 15:43:10 +0900

    level 1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,38 +6,52 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // level 1-1
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        while (true) {
+            // level 1-1
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            sc.nextLine();
 
-        // level 1-2
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char cal = sc.next().charAt(0);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine();
 
-        // level 1-3
-        int result = 0;
-        if (cal == '+') {
-            result = num1 + num2;
-        }
-        else if (cal == '-') {
-            result = num1 - num2;
-        }
-        else if (cal == '*') {
-            result = num1 * num2;
-        }
-        else if (cal == '/') {
-            if (num2 == 0) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            // level 1-2
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char cal = sc.next().charAt(0);
+
+            // level 1-3
+            int result = 0;
+            if (cal == '+') {
+                result = num1 + num2;
+            }
+            else if (cal == '-') {
+                result = num1 - num2;
+            }
+            else if (cal == '*') {
+                result = num1 * num2;
+            }
+            else if (cal == '/') {
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    continue;
+                }
+                else {
+                    result = num1 / num2;
+                }
             }
             else {
-                result = num1 / num2;
+                System.out.println("숫자를 잘못 입력하셨습니다.");
+                continue;
+            }
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String endWord = sc.next();
+
+            if (endWord.equals("exit")) {
+                break;
             }
         }
-        else {
-            System.out.println("숫자를 잘못 입력하셨습니다.");
-        }
-        System.out.println("결과: " + result);
     }
 }
